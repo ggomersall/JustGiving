23c804235b9be2bd2f7355cef9b6c0d757f67e8a
Gareth Gomersall <ggomersall@gmail.com>
2019-05-04 18:11:46 +0100

    added care button that updates state when clicked

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -8,13 +8,28 @@ import './styles.scss';
 class Main extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      likes: 100,
+      care: false
+    }
+    this.handleCareUpdate = this.handleCareUpdate.bind(this);
+  }
+
+  handleCareUpdate() {
+    if (this.state.care === false) {
+
+      this.setState({ likes: this.state.likes + 1, care: !this.state.care }, console.log(this.state))
+    } else {
+      this.setState({likes: this.state.likes - 1, care: !this.state.care }, console.log(this.state))
+    }
   }
 
   render() {
-    const {charityData, donationsData} = this.props
+    const {charityData, donationsData} = this.props;
+    const {likes, care} = this.state;
     return (
       <div className="main__section main_wrapper">
-        <Charity charity={charityData} />
+        <Charity charity={charityData} handleCareUpdate={this.handleCareUpdate} likes={likes} care={care}/>
         <Donations charity={charityData} donations={donationsData} />
       </div>
     ) 
