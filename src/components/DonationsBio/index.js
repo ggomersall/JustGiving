f8f884d6bb30142a25fc601f60624e5a046bdd08
Gareth Gomersall <ggomersall@gmail.com>
2019-05-04 16:12:39 +0100

    added donations card with content and styling + updated donation bio with content and styling

--- src/components/DonationsBio/index.js
+++ src/components/DonationsBio/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import './styles.scss';
+
 const DonationsBio = ({charity}) => {
 
   return(
-    <div class>
-      {charity.pageShortName}
+    <div className="card donation-bio__wrapper">
+      <div className="donation-bio__header">
+        <h3>{charity.name}</h3>
+        <p>{charity.description}</p>
+      </div>
+      <div className="donation-bio__contact">
+        <p>{charity.websiteUrl}</p>
+        <p>{charity.emailAddress}</p>
+        <p>{charity.name} Registered charity number {charity.registrationNumber}</p>
+      </div>
     </div>
   )
 }
